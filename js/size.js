63476b4a314f44c49182d29beb95580afde08219
kenanmusali <textme0306@gmail.com>
2025-12-17 14:35:38 +0400

    Updated

--- js/size.js
+++ js/size.js
@@ -2,15 +2,12 @@ function adjustZoom() {
   const width = window.innerWidth;
   const height = window.innerHeight;
 
-  // First condition: width 1024-1500 AND height 740-1000
   if (width >= 1024 && width <= 1500 && height >= 740 && height <= 1000) {
-    document.body.style.zoom = "0.8"; // 90% (as you mentioned)
+    document.body.style.zoom = "0.8";  
   } 
-  // Second condition: width 750-1670 (no height restriction)
-  else if (width >= 750 && width <= 1670) {
-    document.body.style.zoom = "0.7"; // 80%
+  else if (width >= 1028 && width <= 1670) {
+    document.body.style.zoom = "0.7"; 
   } 
-  // Default
   else {
     document.body.style.zoom = "1";
   }
