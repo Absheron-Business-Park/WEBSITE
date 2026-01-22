e167b757b3043235a36d99bdc00f053d7366be07
kenanmusali <textme0306@gmail.com>
2026-01-22 10:18:13 +0400

    Updated

--- js/size.js
+++ js/size.js
@@ -2,19 +2,29 @@ function adjustZoom() {
   const width = window.innerWidth;
   const height = window.innerHeight;
   const headerGroup = document.querySelector(".Header-Group");
+  let appliedZoom = "1";  
 
-  if (width >= 1024 && width <= 1500 && height >= 720 && height <= 1000) {
-    document.body.style.zoom = "0.8";  
-  } 
-  else if (width >= 1024 && width <= 1500 && height >= 700 && height < 720) {
-    document.body.style.zoom = "0.9";
+  if (width > 1100 && width <= 1295) {
+    appliedZoom = "0.8";
   }
-  else if (width >= 1028 && width <= 1670) {
-    document.body.style.zoom = "0.7"; 
-  } 
-  else {
-    document.body.style.zoom = "1";
+  else if (width >= 1024 && width <= 1500) {
+    if (height >= 720 && height <= 1000) {
+      appliedZoom = "0.85";
+    } 
+    else if (height >= 700 && height < 720) {
+      appliedZoom = "0.8";
+    }
+    else {
+      appliedZoom = "1";
+    }
   }
+  else if (width > 1500 && width <= 1670) {
+    appliedZoom = "0.9";
+  }
+
+  document.body.style.zoom = appliedZoom;
+
+  console.log(`Width: ${width}px, Height: ${height}px, Applied Zoom: ${appliedZoom}`);
 
   if (width >= 1024 && height < 730 && headerGroup) {
     headerGroup.style.height = "85dvh";
