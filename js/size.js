67fe36d976869bc26530f7a200dba60a96f90cec
kenanmusali <textme0306@gmail.com>
2026-01-22 09:43:59 +0400

    Updated

--- js/size.js
+++ js/size.js
@@ -3,10 +3,13 @@ function adjustZoom() {
   const height = window.innerHeight;
   const headerGroup = document.querySelector(".Header-Group");
 
-  if (width >= 1024 && width <= 1500 && height >= 730 && height <= 1000) {
+  if (width >= 1024 && height <= 710) {
+    document.body.style.zoom = "1";
+  }
+  else if (width >= 1024 && width <= 1500 && height > 710 && height <= 1000) {
     document.body.style.zoom = "0.8";
   }
-  else if (width >= 1028 && width <= 1670) {
+  else if (width > 1500 && width <= 1670) {
     document.body.style.zoom = "0.7";
   }
   else {
